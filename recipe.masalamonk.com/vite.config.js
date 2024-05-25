4ceac4b345c1c112a369309a7c1ac1b9b0772b54
Shashank <shashank.aggarwal1@gmail.com>
2024-05-26 02:47:49 +0530

    changed vercel.json to a simpler settting. vite.config.js chnaged to reflect corresponding changes

--- recipe.masalamonk.com/vite.config.js
+++ recipe.masalamonk.com/vite.config.js
@@ -4,6 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api': {
