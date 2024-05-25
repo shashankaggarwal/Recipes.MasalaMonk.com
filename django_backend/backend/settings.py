7d9b8ad4bf4a467ac0cd518a0b05e1c5d4ddb0bf
Shashank <shashank.aggarwal1@gmail.com>
2024-05-25 18:26:58 +0530

    Update API URL and CORS settings

--- django_backend/backend/settings.py
+++ django_backend/backend/settings.py
@@ -119,6 +119,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    'http://157.230.29.35:8000',
+    'https://recipe-masalamonk-cldoyddcm-shashanks-projects-cf0518a2.vercel.app',
+    'https://*.vercel.app',
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
--- recipe.masalamonk.com/vite.config.js
+++ recipe.masalamonk.com/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [svelte()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'http://157.230.29.35:8000',
     },
   },
 });
