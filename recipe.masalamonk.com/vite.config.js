649a637911afbc62882d316db161189583f502b3
Shashank <shashank.aggarwal1@gmail.com>
2024-05-26 05:12:46 +0530

    changed Vercel.json and vite.config.js

--- recipe.masalamonk.com/vite.config.js
+++ recipe.masalamonk.com/vite.config.js
@@ -1,19 +1,33 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist', // Make sure this matches the directory specified in vercel.json
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'), // Main entry point
+      },
+      output: {
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL,
+        target: process.env.VITE_API_URL, // Use during development
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
+  base: '/' // Ideal for deployment to the root of a domain
 });
