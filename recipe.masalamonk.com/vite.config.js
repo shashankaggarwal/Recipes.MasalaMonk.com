7746fa21e71c4f49139ce87f575e937c4299009d
Shashank <shashank.aggarwal1@gmail.com>
2024-05-26 11:47:40 +0530

    Major changes to GitAction Steps, new Json and Vite.Config.

--- recipe.masalamonk.com/vite.config.js
+++ recipe.masalamonk.com/vite.config.js
@@ -3,31 +3,27 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte()  // Svelte plugin integration for Vite
+  ],
   build: {
-    outDir: 'dist', // Make sure this matches the directory specified in vercel.json
+    outDir: 'dist',  // Output directory for build files
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'), // Main entry point
-      },
-      output: {
-        assetFileNames: '[name].[ext]'
-      }
+      input: path.resolve(__dirname, 'index.html')  // Entry point for the application
     }
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+      '@': path.resolve(__dirname, 'src'),  // Alias for src directory
+    }
   },
   server: {
     proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL, // Use during development
+      '/api': {  // Proxy API requests during development
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  base: '/' // Ideal for deployment to the root of a domain
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
